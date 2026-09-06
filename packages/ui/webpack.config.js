26f0dd095136123785cbd4ec6e4204f8e687cebb
Nitin Pathak <nitinpathak211@gmail.com>
2026-09-07 02:13:28 +0530

    chore: prepare UI package for distribution (#12)

--- packages/ui/webpack.config.js
+++ packages/ui/webpack.config.js
@@ -1,41 +1,40 @@
 const path = require ("path");
 
 module.exports = {
-    mode: "production",
-    entry: "./src/index.ts",
-    output: {
-        path: path.resolve(__dirname, "dist"),
-        filename: "index.js",
-        library: {
-            type: "commonjs2"
-        },
-        clean: true,
+  mode: "production",
+  entry: "./src/index.ts",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "index.js",
+    library: {
+        type: "commonjs2"
     },
-    resolve: {
-        extensions: [".ts", ".tsx", ".js"] 
-    },
-    module: {
-        rules : [
-            {
-                test: /\.tsx?$/,
-                exclude: /node_modules/,
-                use: {
-                loader: "ts-loader",
-                    options: {
-                        configFile: path.resolve(__dirname, "tsconfig.json")
-                    }
-                }
-            },
-            {
-                test: /\.css$/,
-                use: ["style-loader", "css-loader"]
-
+    clean: true,
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"] 
+  },
+  module: {
+    rules : [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: {
+        loader: "ts-loader",
+            options: {
+                configFile: path.resolve(__dirname, "tsconfig.json")
             }
-        ]
-    },
-    externals: {
-        react: "react",
-        "react-dom": "react-dom"
-    }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
 
+      }
+    ]
+  },
+  externals: {
+    react: "react",
+    "react-dom": "react-dom"
+}
 }
